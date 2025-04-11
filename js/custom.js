0ec994ffec8e072b879687c8531ebda19193c3ab
sydhussain20 <105807215+sydhussain20@users.noreply.github.com>
2025-04-12 02:58:05 +0530

    Modified forms

--- js/custom.js
+++ js/custom.js
@@ -1,88 +1,103 @@
+
+
 (function ($) {
 
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
 
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
+  // MENU
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
-    $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-          } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-          }
-    });
+  $(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
 
 
-    // HOME SLIDER & COURSES & CLIENTS
-    $('.home-slider').owlCarousel({
-      animateOut: 'fadeOut',
-      items:1,
-      loop:true,
-      dots:false,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-    })
+  // HOME SLIDER & COURSES & CLIENTS
+  $('.home-slider').owlCarousel({
+    items: 1,
+    loop: false,              // No looping
+    autoplay: false,          // No auto sliding
+    mouseDrag: false,         // No dragging
+    touchDrag: false,         // Disable swipe on mobile
+    pullDrag: false,
+    dots: false,
+    nav: false
+  })
 
-    $('.owl-courses').owlCarousel({
-      animateOut: 'fadeOut',
-      loop: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-      dots: false,
-      nav:true,
-      navText: [
-          '<i class="fa fa-angle-left"></i>',
-          '<i class="fa fa-angle-right"></i>'
-      ],
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        1000: {
-          items: 3,
-        }
+  $('.owl-courses').owlCarousel({
+    animateOut: 'fadeOut',
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
       }
-    });
+    }
+  });
 
-    $('.owl-client').owlCarousel({
-      animateOut: 'fadeOut',
-      loop: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        1000: {
-          items: 3,
-        }
+  $('.owl-client').owlCarousel({
+    animateOut: 'fadeOut',
+    loop: true,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
       }
+    }
+  });
+
+
+  // SMOOTHSCROLL
+  $(function () {
+    $('.custom-navbar a, #home a').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
     });
-
-
-    // SMOOTHSCROLL
-    $(function() {
-      $('.custom-navbar a, #home a').on('click', function(event) {
-        var $anchor = $(this);
-          $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-          }, 1000);
-            event.preventDefault();
-      });
-    });  
+  });
 
 })(jQuery);
+
+document.querySelectorAll('.item[data-course]').forEach(item => {
+  item.addEventListener('click', function () {
+    console.log('click')
+    const course = this.getAttribute('data-course');
+    const select = document.getElementById('course-select');
+    if (select) {
+      select.value = course;
+      document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
